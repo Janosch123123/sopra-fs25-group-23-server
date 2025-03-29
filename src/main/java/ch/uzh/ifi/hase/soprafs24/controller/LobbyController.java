a055ee08ab7cf769184aaff64ab0aacfa70a30de
LuckyLuke637 <lukebenjamin.fohringer@uzh.ch>
2025-03-29 13:48:35 +0100

    created a basic lobby entity and started on lobby service #66

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
@@ -0,0 +1,31 @@
+package ch.uzh.ifi.hase.soprafs24.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.sql.Date;
+
+/**
+ * Lobby Controller
+ * This class is responsible for handling all REST request that are related to
+ * the lobby.
+ * The controller will receive the request and delegate the execution to the
+ * LobbyService and finally return the result.
+ */
+@RestController
+public class LobbyController {
+
+  private final LobbyService lobbyService;
+
+  LobbyController(LobbyService lobbyService) {
+    this.lobbyService = lobbyService;
+  }
+
+  @PostMapping("/lobbies")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+}
